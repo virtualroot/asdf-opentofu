6ad03be2b86f010748fa95838cb60b7f45b7d418
Alejandro Lazaro <alazaro@werpo.com.ar>
2023-10-04 16:11:22 +0200

    Releases are .zip

--- lib/utils.bash
+++ lib/utils.bash
@@ -42,7 +42,7 @@ download_release() {
 	filename="$2"
 
 	# TODO: Adapt the release URL convention for opentofu
-	url="$GH_REPO/archive/v${version}.tar.gz"
+	url="$GH_REPO/archive/v${version}.zip"
 
 	echo "* Downloading $TOOL_NAME release $version..."
 	curl "${curl_opts[@]}" -o "$filename" -C - "$url" || fail "Could not download $url"
